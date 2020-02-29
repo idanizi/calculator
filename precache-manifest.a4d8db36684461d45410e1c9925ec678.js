f4a88b19415f6b178ba139e845d5e5067a90a796
Idan Izicovich <idanizi@gmail.com>
2020-02-29 03:47:42 +0200

    Updates

---
+++ precache-manifest.a4d8db36684461d45410e1c9925ec678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0509e0b4adceae15268e907f73c51b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "79fb2c625f5d1dac5b36",
+    "url": "/calculator/static/css/main.d7e662af.chunk.css"
+  },
+  {
+    "revision": "82817bfa75675bbd1bff",
+    "url": "/calculator/static/js/2.5e41bd14.chunk.js"
+  },
+  {
+    "revision": "8f8eea7f0602a56640cf02b0537fe791",
+    "url": "/calculator/static/js/2.5e41bd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fb2c625f5d1dac5b36",
+    "url": "/calculator/static/js/main.e1fa9d2a.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
