dd73786327b4b4c4a2a5bf2e936bef7c8b058a2f
Idan Izicovich <idanizi@gmail.com>
2020-02-29 03:48:17 +0200

    readme

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Buttons from './components/Buttons'
 
 function App() {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
-      <Box>
-        <Screen />
-        <Buttons />
+    <main>
+      <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+        <Box style={{
+          background: "#aaa",
+          padding: '0.7em',
+          borderRadius: "5%"
+        }}>
+          <Screen />
+          <Buttons />
+        </Box>
       </Box>
-    </Box>
+      <footer>© 2020 Idan Izicovich <span role="img">😎</span></footer>
+    </main>
   );
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -3,13 +3,23 @@ import Box from '@material-ui/core/Box'
 import { useStoreState } from "easy-peasy";
 export default function () {
 
-    const currentNumber = useStoreState(state => state.currentNumber)
+  const currentNumber = useStoreState(state => state.currentNumber)
 
-    return (
-      <Box style={{ border: "1px black solid", padding:"0.5em", margin:"2vh 0" }}>
-        <Box style={{ border: "1px black solid" }}>
-          {currentNumber}
-        </Box>
+  return (
+    <Box style={{
+      border: "1px black solid",
+      padding: "0.5em",
+      margin: "2vh 0",
+      borderRadius: "5% / 20%",
+      background: "#333"
+    }}>
+      <Box style={{
+        border: "1px black solid",
+        padding: "0.2em",
+        background: "#eee"
+      }}>
+        {currentNumber}
       </Box>
-    );
-  }
+    </Box>
+  );
+}
